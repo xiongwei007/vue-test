a366961bb7be1178af834ade67540947dfe6f6f3
xiangwei <1020593327@qq.com>
2020-04-29 14:59:35 +0800

    新建首页

---
+++ my-firstvue/src/pages/home/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>hello---{{homes}}</div>
+</template>
+<script>
+export default {
+    name:'Home',
+    data() {
+        return {
+            homes:'12234'
+        }
+    }
+}
+</script>
